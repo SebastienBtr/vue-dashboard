5cb3f23cde566a6b0040ebea17561881b38b1ac6
SebastienBtr <sebastien29520@gmail.com>
2020-12-11 14:36:31 +0100

    move dependencies into dev dependencies

--- vue-dashboard/.eslintrc.js
+++ vue-dashboard/.eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': 'off',
+    'import/no-extraneous-dependencies': 'off',
   },
 };
