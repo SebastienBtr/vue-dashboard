f97c1b6ff7bdfe06cac9cb9e2148c3d7bc427f9a
SebastienBtr <sebastien29520@gmail.com>
2020-12-11 16:25:35 +0100

    remove unuse line

--- examples/vue2-example/src/main.ts
+++ examples/vue2-example/src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import VueDashboard from 'vue-dashboard-vd';
 import App from './App.vue';
 import router from './router';
-// import 'vue-dashboard-vd/dist/vue-dashboard-vd.css';
 import 'bulma/css/bulma.min.css';
 import '@mdi/font/css/materialdesignicons.min.css';
 import HeaderItems from './components/HeaderItems.vue';
