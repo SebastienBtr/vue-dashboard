2e12a6f633deedb0927dd28395ea3133f0b677c7
SebastienBtr <sebastien29520@gmail.com>
2020-12-12 19:13:33 +0100

    edit example

--- examples/vue2-example/src/App.vue
+++ examples/vue2-example/src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <vd-dashboard
-      content="router-view"
-      sidebarHeaderHeight="175px"
-      headerItems="header-items"
-      sidebarHeader="sidebar-header"
-      sidebarItems="sidebar-items"
+    <VdDashboard
       pageBackground="#f8f8f8"
+      sidebarHeaderHeight="175px"
       headerBackground="white"
       sidebarBackground="white"
-    ></vd-dashboard>
+    >
+      <template v-slot:main-content>
+        <router-view />
+      </template>
+      <template v-slot:header-content>
+        <HeaderItems></HeaderItems>
+      </template>
+      <template v-slot:sidebar-header>
+        <SidebarHeader></SidebarHeader>
+      </template>
+      <template v-slot:sidebar-content>
+        <SidebarItems></SidebarItems>
+      </template>
+    </VdDashboard>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HeaderItems from './components/HeaderItems.vue';
+import SidebarHeader from './components/SidebarHeader.vue';
+import SidebarItems from './components/SidebarItems.vue';
+
+@Component({
+  components: {
+    HeaderItems,
+    SidebarHeader,
+    SidebarItems,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style>
 #app {
--- examples/vue2-example/src/main.ts
+++ examples/vue2-example/src/main.ts
@@ -4,15 +4,8 @@ import App from './App.vue';
 import router from './router';
 import 'bulma/css/bulma.min.css';
 import '@mdi/font/css/materialdesignicons.min.css';
-import HeaderItems from './components/HeaderItems.vue';
-import SidebarHeader from './components/SidebarHeader.vue';
-import SidebarItems from './components/SidebarItems.vue';
 
 Vue.config.productionTip = false;
-
-Vue.component('header-items', HeaderItems);
-Vue.component('sidebar-items', SidebarItems);
-Vue.component('sidebar-header', SidebarHeader);
 
 Vue.use(VueDashboard);
 
